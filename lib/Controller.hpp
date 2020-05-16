17613ca253febde2ee1f6ae56f5ad2d593f531fc
pluot <>
2020-05-16 12:40:55 -0400

    default deadzone reduced to .5

--- lib/Controller.hpp
+++ lib/Controller.hpp
@@ -28,7 +28,7 @@
 
 #define MIN_DEADZONE        ( 328 )     // 328   / 32767 ~ 0.01
 #define MAX_DEADZONE        ( 32439 )   // 32439 / 32767 ~ 0.99
-#define DEFAULT_DEADZONE    ( 25000 )   // 25000 / 32767 ~ 0.76
+#define DEFAULT_DEADZONE    ( 16400 )   // 16400 / 32767 ~ 0.50
 
 #define MAP_PRESERVE_DIRS   ( 0x01u )
 #define MAP_CONTINUOUSLY    ( 0x02u )
